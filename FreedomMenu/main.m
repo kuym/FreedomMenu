ab9b4718b8a93f465f7884f6d9bddfee01c0f2d2
Kuy Mainwaring <kuy@outbreak.co>
2013-12-28 14:32:24 -0800

    HTTP check functionality added, parametric rendering of meter, intermediate state added. No username/password settings, no settings UI at this point.

--- FreedomMenu/main.m
+++ FreedomMenu/main.m
@@ -1,12 +1,46 @@
 #include <Cocoa/Cocoa.h>
 #include <CoreGraphics/CoreGraphics.h>
 
+
+@implementation NSString (NSString_Extended)
+
+- (NSString*)urlencode
+{
+	NSMutableString* output = [NSMutableString string];
+	const unsigned char* source = (const unsigned char*)[self UTF8String];
+	size_t sourceLen = strlen((const char*)source);
+	for(int i = 0; i < sourceLen; ++i)
+	{
+		const unsigned char thisChar = source[i];
+		if (thisChar == ' ')
+		{
+			[output appendString:@"+"];
+		}
+		else if(	thisChar == '.' || thisChar == '-' || thisChar == '_' || thisChar == '~' ||
+					(thisChar >= 'a' && thisChar <= 'z') ||
+					(thisChar >= 'A' && thisChar <= 'Z') ||
+					(thisChar >= '0' && thisChar <= '9'))
+		{
+			[output appendFormat:@"%c", thisChar];
+		}
+		else
+			[output appendFormat:@"%%%02X", thisChar];
+	}
+	return(output);
+}
+@end
+
+////////////////////////////////////////////////////////////////
+
+
 @class MenubarController;
+@class UsageCheckController;
 
 @interface FreedomMenuApp: NSObject <NSApplicationDelegate>
 {
 @private
-	MenubarController*	_menubarController;
+	MenubarController*		_menubarController;
+	UsageCheckController*	_checkController;
 }
 
 @end
@@ -21,7 +55,10 @@
 {
 @private
 	MenubarView*	_menubarView;
+	NSMenuItem*		_detailItem;
 }
+
+- (void)updateUsedQuotient:(float)quotient;
 
 //@property (nonatomic) BOOL hasActiveIcon;
 @property (nonatomic, strong, readonly) MenubarView* statusItemView;
@@ -34,27 +71,80 @@
 ////////////////////////////////////////////////////////////////
 
 
+@interface UsageCheckController: NSObject <NSURLConnectionDelegate, NSURLConnectionDataDelegate>
+{
+@private
+	NSString*			_username;
+	NSString*			_password;
+	
+	NSTimer*			_timer;
+	NSTimeInterval		_timerPeriod;
+	BOOL				_updateImminent;
+	
+	NSURLConnection*	_connection;
+	
+	NSMutableData*		_response;
+	
+	float				_usedQuotient;
+	SEL					_usedQuotientOnChange;
+	id					_usedQuotientOnChangeTarget;
+}
+
+- (id)initWithUsername:(NSString*)username password:(NSString*)password;
+
+- (void)setUsername:(NSString*)username;
+- (void)setPassword:(NSString*)password;
+
+- (void)onUsedQuotientChanged:(SEL)handler target:(id)target;
+
+- (void)setUpdatePeriod:(NSTimeInterval)interval;
+
+@property (nonatomic, setter = setUsername:) NSString* username;
+@property (nonatomic, setter = setPassword:) NSString* password;
+@property (nonatomic, readonly) float usedQuotient;
+@property (nonatomic, readonly) NSTimeInterval updatePeriod;
+
+
+@end
+
+
+////////////////////////////////////////////////////////////////
+
+typedef enum
+{
+	MenubarViewImageSlot_normal = 0,
+	MenubarViewImageSlot_highlight = 1,
+	MenubarViewImageSlot_indeterminate_normal = 2,
+	MenubarViewImageSlot_indeterminate_highlight = 3,
+	
+	MenubarViewImageSlot__pastEnd
+} MenubarViewImageSlot;
+
 @interface MenubarView: NSView <NSMenuDelegate>
 {
 @private
-	NSImage*		_image;
-	NSImage*		_highlightImage;
+	NSImage*		_image[4];
 	NSStatusItem*	_statusItem;
 	BOOL			_isHighlighted;
 	SEL				_action;
 	id				_target;		//__unsafe_unretained
+	
+	float			_displayedQuotient;
 }
 
-- (id)initWithStatusItem:(NSStatusItem *)statusItem;
+- (id)initWithStatusItem:(NSStatusItem*)statusItem;
 
-- (void)setImage:(NSImage*)newImage;
-- (void)setHighlightImage:(NSImage*)newImage;
+- (void)setImage:(NSImage*)newImage forSlot:(MenubarViewImageSlot)slot;
+//- (void)setHighlightImage:(NSImage*)newImage;
 - (void)setMenu:(NSMenu*)menu;
 - (void)setHighlighted:(BOOL)newFlag;
 - (void)setAction:(SEL)action onTarget:(id)target;
 
+- (void)setDisplayedQuotient:(float)quotient;
+
 @property (nonatomic, strong, readonly) NSStatusItem* statusItem;
 @property (nonatomic, setter = setHighlighted:) BOOL isHighlighted;
+@property (nonatomic, setter = setDisplayedQuotient:) float displayedQuotient;
 
 @end
 
@@ -65,9 +155,18 @@
 
 @implementation FreedomMenuApp
 
-- (void)applicationDidFinishLaunching:(NSNotification* )aNotification
+- (void)applicationDidFinishLaunching:(NSNotification*)aNotification
 {
 	_menubarController = [[MenubarController alloc] init];
+	
+	_checkController = [[UsageCheckController alloc] initWithUsername:@"YOURUSERNAME" password:@"YOURPASSWORD"];
+	
+	[_checkController onUsedQuotientChanged:@selector(onQuotientUpdate) target:self];
+}
+
+- (void)onQuotientUpdate
+{
+	[_menubarController updateUsedQuotient:[_checkController usedQuotient]];
 }
 
 @end
@@ -86,13 +185,16 @@
 	
 	_menubarView = [[MenubarView alloc] initWithStatusItem:statusItem];
 	
-	[_menubarView setImage:[NSImage imageNamed:@"Status"]];
-	[_menubarView setHighlightImage:[NSImage imageNamed:@"StatusHighlighted"]];
+	[_menubarView setImage:[NSImage imageNamed:@"Status"] forSlot:MenubarViewImageSlot_normal];
+	[_menubarView setImage:[NSImage imageNamed:@"StatusHighlighted"] forSlot:MenubarViewImageSlot_highlight];
+	[_menubarView setImage:[NSImage imageNamed:@"StatusInd"] forSlot:MenubarViewImageSlot_indeterminate_normal];
+	[_menubarView setImage:[NSImage imageNamed:@"StatusIndHighlighted"] forSlot:MenubarViewImageSlot_indeterminate_highlight];
+	
 	//[_menubarView setAction:@selector(doStuff:) onTarget:self];
 	
 	//build a menu
 	NSMenu* menu = [[NSMenu alloc] initWithTitle:@"FreedomMenu"];
-	[menu insertItemWithTitle:@"Used: 1% (35MB of 500MB)" action:nil keyEquivalent:@"" atIndex:0];
+	_detailItem = [menu insertItemWithTitle:@"Used: Currently unknown." action:nil keyEquivalent:@"" atIndex:0];
 	[[menu insertItemWithTitle:@"FreedomPop Account..." action:@selector(goToAccount:) keyEquivalent:@"" atIndex:1] setTarget:self];
 	[menu insertItem:[NSMenuItem separatorItem] atIndex:2];
 	[[menu insertItemWithTitle:@"Settings..." action:@selector(showSettings:) keyEquivalent:@"" atIndex:3] setTarget:self];
@@ -109,15 +211,24 @@
 	//[_menubarView.statusItem popUpStatusItemMenu:[_menubarView.statusItem menu]];
 }*/
 
+- (void)updateUsedQuotient:(float)quotient
+{
+	[_menubarView setDisplayedQuotient:quotient];
+	
+	if(quotient >= 0.f)
+		[_detailItem setTitle:[NSString stringWithFormat:@"Used: %3.1f%%", quotient * 100.f]];
+	else
+		[_detailItem setTitle:@"Used: Currently unknown."];
+}
+
 - (IBAction)goToAccount:(id)sender
 {
-	printf("Going to account...\n");
 	[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://www.freedompop.com/acct_usage.htm"]];
 }
 
 - (IBAction)showSettings:(id)sender
 {
-	printf("Showing settings...\n");
+	printf("Would show settings...\n");
 }
 
 - (void)dealloc
@@ -128,13 +239,207 @@
 @end
 
 
+////////////////////////////////////////////////////////////////
 
 
+@implementation UsageCheckController
+
+@synthesize username = _username;
+@synthesize password = _password;
+@synthesize usedQuotient = _usedQuotient;
+@synthesize updatePeriod = _timerPeriod;
+
+- (id)init
+{
+	_username = nil;
+	_password = nil;
+	_response = nil;
+	_usedQuotient = -1.f;
+	_updateImminent = NO;
+	
+	_timerPeriod = 3600.f;	// update by default each hour
+	
+	return(self);
+}
+
+- (id)initWithUsername:(NSString *)username password:(NSString *)password
+{
+	_username = username;
+	_password = password;
+	_response = nil;
+	_usedQuotient = -1.f;
+	_updateImminent = NO;
+	
+	_timerPeriod = 3600.f;	// update by default each hour
+	[self checkNow];
+	
+	return(self);
+}
+
+- (void)setUsername:(NSString*)username
+{
+	_username = username;
+	[self checkNow];
+}
+- (void)setPassword:(NSString*)password
+{
+	_password = password;
+	[self checkNow];
+}
+
+- (void)onUsedQuotientChanged:(SEL)handler target:(id)target
+{
+	_usedQuotientOnChange = handler;
+	_usedQuotientOnChangeTarget = target;
+}
+
+- (void)setUpdatePeriod:(NSTimeInterval)interval
+{
+	_timerPeriod = interval;
+	if(!_updateImminent)
+		[self scheduleCheckIn:_timerPeriod];
+}
+
+- (void)checkNow
+{
+	_updateImminent = YES;
+	_timer = [NSTimer scheduledTimerWithTimeInterval:1.f target:self selector:@selector(performCheck:) userInfo:self repeats:NO];
+}
+
+- (void)scheduleCheckIn:(NSTimeInterval)seconds
+{
+	if(!_updateImminent)
+	{
+		if((_timer != nil) && [_timer isValid])
+			[_timer invalidate];
+		_timer = [NSTimer scheduledTimerWithTimeInterval:seconds target:self selector:@selector(performCheck:) userInfo:self repeats:NO];
+	}
+}
+
+- (void)performCheck:(NSTimer*)timer
+{
+	_updateImminent = NO;
+	
+	[self scheduleCheckIn:_timerPeriod];
+	
+	//printf("checking now...\n");
+	
+	if((_username == nil) || (_password == nil))
+	{
+		//printf("unable to check, no username or password.\n");
+		return;
+	}
+		
+	NSMutableURLRequest* request = [[NSMutableURLRequest alloc] init];
+	[request setURL:[NSURL URLWithString:@"https://www.freedompop.com/login.htm"]];
+	[request setHTTPMethod:@"POST"];
+	[request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+	
+	NSString* post = [NSString stringWithFormat: @"signin-username-full=%@&signin-password-full=%@&destinationURL=http://www.freedompop.com/acct_usage.htm&requestOrigin=",
+							[_username urlencode],
+							[_password urlencode]
+						];
+	
+	NSData* postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+	
+	NSString* postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+	[request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+	[request setHTTPBody:postData];
+	
+	_response = nil;
+	_connection = [NSURLConnection connectionWithRequest:request delegate:self];
+}
+
+- (void)connection:(NSURLConnection*)connection didFailWithError:(NSError*)error
+{
+	//printf("error.\n");
+}
+
+- (NSURLRequest*)connection:(NSURLConnection*)connection willSendRequest:(NSURLRequest*)request redirectResponse:(NSURLResponse*)response
+{
+	//printf("will redirect.\n");
+	
+	return(request);
+}
+- (void)connection:(NSURLConnection*)connection didReceiveResponse:(NSHTTPURLResponse*)response
+{
+	//printf("got response.\n");
+	
+	NSNumber* length = (NSNumber*)[[response allHeaderFields] objectForKey:@"Content-Length"];
+	size_t responseLength = (length != nil)? [length intValue] : 0;
+	
+	if((_response == nil) && (responseLength > 0))
+		_response = [[NSMutableData alloc] initWithCapacity:responseLength];
+}
+
+- (void)connection:(NSURLConnection*)connection didReceiveData:(NSData*)data
+{
+	//printf("rxd data, %lu bytes.\n", (unsigned long)[data length]);
+	
+	if(_response == nil)
+		_response = [[NSMutableData alloc] initWithCapacity:[data length]];
+	
+	[_response appendData:data];
+}
+
+- (void)connection:(NSURLConnection*)connection	didSendBodyData:(NSInteger)bytesWritten
+												totalBytesWritten:(NSInteger)totalBytesWritten
+												totalBytesExpectedToWrite:(NSInteger)totalBytesExpectedToWrite
+{
+	//printf("progress %li %li %li.\n", bytesWritten, totalBytesWritten, totalBytesExpectedToWrite);
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection*)connection
+{
+	//printf("finished.\n");
+	
+	_connection = nil;
+	
+	char const* searchString = "<div class=\"colorBar\" style=\"width:";
+	size_t searchStringLen = strlen(searchString);
+	NSData* htmlNodeOfInterest = [NSData dataWithBytes:searchString length:searchStringLen];
+	NSRange found = [_response rangeOfData:htmlNodeOfInterest options:0 range:NSMakeRange(0, [_response length])];
+	
+	if(found.location != NSNotFound)
+	{
+		//printf("found at %lu, %lu\n", (unsigned long)found.location, (unsigned long)found.length);
+		
+		NSData* endMarker = [NSData dataWithBytes:"%" length:1];
+		size_t objectivePosition = found.location + searchStringLen;
+		NSRange foundEnd = [_response rangeOfData:endMarker options:0 range:NSMakeRange(objectivePosition, [_response length] - objectivePosition)];
+		
+		// accept up to 10 decimal digits
+		if((foundEnd.location != NSNotFound) && ((foundEnd.location - objectivePosition) < 10))
+		{
+			NSData* subData = [_response subdataWithRange:NSMakeRange(found.location, (foundEnd.location - found.location))];
+			
+			int usedPercentage = [[[NSString alloc] initWithData:subData encoding:NSUTF8StringEncoding] intValue];
+			
+			if((usedPercentage >= 0) && (usedPercentage <= 100))
+			{
+				_usedQuotient = (((float)usedPercentage) / 100.f);
+				
+				//printf("Used quotient is %f\n", _usedQuotient);
+				
+				if(_usedQuotientOnChange != nil)
+					[NSApp sendAction:_usedQuotientOnChange to:_usedQuotientOnChangeTarget from:self];
+			}
+		}
+	}
+	
+	_response = nil;
+}
+
+
+@end
+
+////////////////////////////////////////////////////////////////
 
 
 @implementation MenubarView
 
 @synthesize statusItem = _statusItem;
+@synthesize displayedQuotient = _displayedQuotient;
 
 - (id)initWithStatusItem:(NSStatusItem*)statusItem
 {
@@ -142,6 +447,8 @@
 	CGFloat itemHeight = [[NSStatusBar systemStatusBar] thickness];
 	NSRect itemRect = NSMakeRect(0.0, 0.0, itemWidth, itemHeight);
 	self = [super initWithFrame:itemRect];
+	
+	_displayedQuotient = -1.f;	// indeterminate
 
 	if(self != nil)
 	{
@@ -154,7 +461,6 @@
 - (void)drawRect:(NSRect)dirtyRect
 {
 	[self.statusItem drawStatusBarBackgroundInRect:dirtyRect withHighlight:_isHighlighted];
-	
 	
 	/*
 	CGAffineTransform mtx = CGAffineTransformMake(24.f, 0.f, 0.f, 24.f, 0.f, 0.f);
@@ -169,7 +475,8 @@
 	CGContextStrokePath(context);
 	*/
 
-	NSImage* icon = _isHighlighted ? _highlightImage : _image;
+	NSImage* icon = (_displayedQuotient >= 0.f)? (_isHighlighted ? _image[MenubarViewImageSlot_highlight] : _image[MenubarViewImageSlot_normal])
+				: (_isHighlighted ? _image[MenubarViewImageSlot_indeterminate_highlight] : _image[MenubarViewImageSlot_indeterminate_normal]);
 	
 	if(icon)
 	{
@@ -180,17 +487,28 @@
 		NSPoint iconPoint = NSMakePoint(iconX, iconY);
 
 		CGContextRef context = [[NSGraphicsContext currentContext] graphicsPort];
-
+	
 		[icon drawAtPoint:iconPoint fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
 		
-		if(_isHighlighted)	CGContextSetRGBFillColor(context, 0.99f, 0.99f, 0.99f, 1.f);
-		else				CGContextSetRGBFillColor(context, 0.08f, 0.08f, 0.08f, 1.f);
-		CGContextFillRect(context, CGRectMake(iconX + 3.f, iconY + 3.f, 13.f, 5.f));
+		if(_displayedQuotient >= 0.f)
+		{
+			if(_isHighlighted)	CGContextSetRGBFillColor(context, 0.99f, 0.99f, 0.99f, 1.f);
+			else				CGContextSetRGBFillColor(context, 0.08f, 0.08f, 0.08f, 1.f);
+			
+			// display used quotient of 0.0 as full and 1.0 as empty, like a battery charge meter
+			CGContextFillRect(context, CGRectMake(iconX + 3.f, iconY + 3.f, 19.f * (1.f - _displayedQuotient), 5.f));
+		}
 	}
 }
 
 - (void)setAction:(SEL)action onTarget:(id)target
 {
+}
+
+- (void)setDisplayedQuotient:(float)quotient
+{
+	_displayedQuotient = quotient;
+	[self setNeedsDisplay:YES];
 }
 
 - (void)mouseDown:(NSEvent*)theEvent
@@ -237,22 +555,14 @@
 	_target = target;
 }*/
 
-- (void)setImage:(NSImage*)newImage
+- (void)setImage:(NSImage*)newImage forSlot:(MenubarViewImageSlot)slot
 {
-	if(_image != newImage)
+	if(slot >= MenubarViewImageSlot__pastEnd)
+		return;
+	if(_image[slot] != newImage)
 	{
-		_image = newImage;
+		_image[slot] = newImage;
 		[self setNeedsDisplay:YES];
-	}
-}
-
-- (void)setHighlightImage:(NSImage*)newImage
-{
-	if(_highlightImage != newImage)
-	{
-		_highlightImage = newImage;
-		if(_isHighlighted)
-			[self setNeedsDisplay:YES];
 	}
 }
 
@@ -269,7 +579,7 @@
 ////////////////////////////////////////////////////////////////
 
 
-int main(int argc, const char * argv[])
+int main(int argc, const char*  argv[])
 {
 	FreedomMenuApp* application = [[FreedomMenuApp alloc] init];
 
